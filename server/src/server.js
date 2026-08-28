ac8b5f341c130723cf0f6739d3c4bfd92f06c79f
Mahantesh S <85508987+mahanthu8@users.noreply.github.com>
2026-08-29 01:04:44 +0530

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -14,6 +14,11 @@ export const db = mysql.createPool({
   password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'portfolio_db',
   port: Number(process.env.DB_PORT || 3306),
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
